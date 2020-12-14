7101aaeee56307ebe80e1710381c314c1f8de9c0
Bintang Wibawa Mukti <bintang.wib@gmail.com>
2020-12-14 10:12:57 +0700

    update user & package controller
    1. tambah get users dan get user by id
    
    2. tambah patch (claim) package
    
    3. update dokumentasi

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,17 +4,23 @@ const { signToken } = require('../helpers/jwt')
 
 class UserController {
   static async register(req, res, next) {
-    const { name, password, email } = req.body
-    const payload = { name, password, email }
+    const { name, password, email, unit } = req.body
+    const payload = { name, password, email, unit }
     try {
-      const user = await User.create(payload)
-      res.status(201).json({id: user.id, name: user.name, email: user.email, role: user.role})
+      const user = await User.create(payload, { attributes: ['id', 'name', 'email', 'unit'], returning: true })
+      const createdUser = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        unit: user.unit
+      }
+      res.status(201).json(createdUser)
     } catch (err) {
       next(err)
     }
   }
 
-  static async login(req, res, next) {
+  static async loginUser(req, res, next) {
     const { password, email } = req.body
     try {
       // const user = await User.create(payload)
@@ -24,15 +30,80 @@ class UserController {
         const user = await User.findOne({ where: { email: email }})
         if (!user) {
           throw { msg: 'Email/password is wrong!', status: 401}
-        } else if (!checkPassword(password, user.password)) {
-          throw { msg: 'Email/password is wrong!', status: 401}
-        } else {
+        } else if (user.role === 'customer' && checkPassword(password, user.password)) {
           const payload = {
             email: user.email,
             role: user.role
           }
           const access_token = signToken(payload)
-          res.status(200).json({ access_token })
+          res.status(200).json({ ...payload, access_token })
+        } else {
+          throw { msg: 'Email/password is wrong!', status: 401}
+        }
+      }
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async loginAdmin(req, res, next) {
+    const { password, email } = req.body
+    try {
+      // const user = await User.create(payload)
+      if (!password && !email) {
+        throw { msg: 'Password and email cannot be empty!', status: 400 }
+      } else {
+        const user = await User.findOne({ where: { email: email }})
+        if (!user) {
+          throw { msg: 'Email/password is wrong!', status: 401}
+        } else if (user.role === 'admin' && checkPassword(password, user.password)) {
+          const payload = {
+            email: user.email,
+            role: user.role
+          }
+          const access_token = signToken(payload)
+          res.status(200).json({ ...payload, access_token })
+        } else {
+          throw { msg: 'Email/password is wrong!', status: 401}
+        }
+      }
+    } catch (err) {
+      next(err)
+    }
+  }
+  
+  static async getAllUsers(req, res, next) {
+    try {
+      const users = await User.findAll({ where: { role: 'customer' }, 
+        order: [
+          ['createdAt', 'DESC']
+        ],
+        attributes: ['id', 'name', 'email', 'unit']
+      })
+      res.status(200).json(users)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async getUserById(req, res, next) {
+    const UserId = +req.params.id
+    const { id, role } = req.userLoggedIn
+    try {
+      if (isNaN(+id)) {
+        throw { msg: 'user ID is not valid!', status: 400 }
+      } else {
+        if (UserId !== id && role !== 'admin') {
+          throw { msg: 'not authorized!', status: 400 }
+        } else {
+          const user = await User.findByPk(id, 
+            { attributes: ['id', 'name', 'email', 'role', 'createdAt', 'updatedAt', 'unit'] 
+          })
+          if (!user) {
+            throw { msg: 'user not found!', status: 404 }
+          } else {
+            res.status(200).json(user)
+          }
         }
       }
     } catch (err) {
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 const UserController = require('../controllers/UserController')
+const adminAuthorization = require('../middlewares/adminAuthorization')
+
+router.get('/', adminAuthorization, UserController.getAllUsers)
+router.get('/:id', UserController.getUserById)
 
 
 module.exports = router
