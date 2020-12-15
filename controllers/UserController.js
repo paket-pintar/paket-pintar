c49caf3c1257104ddfe8c37fcbdc49bcd285289f
mhmdjundana <mhmd.jundana@gmail.com>
2020-12-15 15:54:23 +0700

    add_push_notification

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,7 @@
 const { User } = require('../models')
 const { checkPassword } = require('../helpers/bcrypt')
 const { signToken } = require('../helpers/jwt')
+const axios = require('axios')
 
 class UserController {
   static async register(req, res, next) {
@@ -27,9 +28,9 @@ class UserController {
       if (!password && !email) {
         throw { msg: 'Password and email cannot be empty!', status: 400 }
       } else {
-        const user = await User.findOne({ where: { email: email }})
+        const user = await User.findOne({ where: { email: email } })
         if (!user) {
-          throw { msg: 'Email/password is wrong!', status: 401}
+          throw { msg: 'Email/password is wrong!', status: 401 }
         } else if (user.role === 'customer' && checkPassword(password, user.password)) {
           const payload = {
             email: user.email,
@@ -40,7 +41,7 @@ class UserController {
           const access_token = signToken(payload)
           res.status(200).json({ ...payload, access_token })
         } else {
-          throw { msg: 'Email/password is wrong!', status: 401}
+          throw { msg: 'Email/password is wrong!', status: 401 }
         }
       }
     } catch (err) {
@@ -55,9 +56,9 @@ class UserController {
       if (!password && !email) {
         throw { msg: 'Password and email cannot be empty!', status: 400 }
       } else {
-        const user = await User.findOne({ where: { email: email }})
+        const user = await User.findOne({ where: { email: email } })
         if (!user) {
-          throw { msg: 'Email/password is wrong!', status: 401}
+          throw { msg: 'Email/password is wrong!', status: 401 }
         } else if (user.role === 'admin' && checkPassword(password, user.password)) {
           const payload = {
             email: user.email,
@@ -66,24 +67,25 @@ class UserController {
           const access_token = signToken(payload)
           res.status(200).json({ ...payload, access_token })
         } else {
-          throw { msg: 'Email/password is wrong!', status: 401}
+          throw { msg: 'Email/password is wrong!', status: 401 }
         }
       }
     } catch (err) {
       next(err)
     }
   }
-  
+
   static async getAllUsers(req, res, next) {
     try {
-      const users = await User.findAll({ where: { role: 'customer' }, 
+      const users = await User.findAll({
+        where: { role: 'customer' },
         order: [
           ['createdAt', 'DESC']
         ],
         attributes: ['id', 'name', 'email', 'unit']
       })
       if (users.length === 0) {
-        throw { msg: 'there is no user in the list.', status: 200}
+        throw { msg: 'there is no user in the list.', status: 200 }
       } else {
         res.status(200).json(users)
       }
@@ -100,9 +102,10 @@ class UserController {
         throw { msg: 'user ID is not valid!', status: 400 }
       } else {
         if (UserId == id || role === 'admin') {
-          const user = await User.findByPk(id, 
-            { attributes: ['id', 'name', 'email', 'role', 'createdAt', 'updatedAt', 'unit'] 
-          })
+          const user = await User.findByPk(id,
+            {
+              attributes: ['id', 'name', 'email', 'role', 'createdAt', 'updatedAt', 'unit']
+            })
           if (!user) {
             throw { msg: 'user not found!', status: 404 }
           } else {
@@ -114,6 +117,53 @@ class UserController {
       }
     } catch (err) {
       next(err)
+    }
+  }
+
+  static async registerToken(req, res, next) {
+    const id = req.params.id
+    const userToken = req.body.userToken
+
+    try {
+      await User.update({ userToken }, { where: { id } })
+      // console.log(success);
+      res.status(200).json({ msg: 'Register user token success!' })
+    } catch (error) {
+      console.log(error);
+      next(error)
+    }
+  }
+
+  static async sendNotification(req, res, next) {
+    let { description, sender, userId } = req.body
+    // console.log('req.body:', description, sender, userId);
+    try {
+      let user = await User.findOne({ where: { id: userId } })
+      let token = user.userToken
+
+      const message = {
+        to: token,
+        sound: 'default',
+        title: `Kiriman dari: ${sender}`,
+        body: description,
+        data: { description, sender },
+      };
+      let dataFeedback = await axios({
+        url: 'https://exp.host/--/api/v2/push/send',
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Accept-encoding': 'gzip, deflate',
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify(message),
+      });
+      // console.log(dataFeedback);
+      // console.log('dataFeedback.config', dataFeedback.config.data);
+      res.status(200).json(dataFeedback.config.data)
+    } catch (error) {
+      console.log(error);
+      next(error)
     }
   }
 }
--- migrations/20201211030040-create-user.js
+++ migrations/20201211030040-create-user.js
@@ -21,6 +21,9 @@ module.exports = {
       role: {
         type: Sequelize.STRING
       },
+      userToken: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
